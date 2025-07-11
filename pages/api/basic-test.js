164b4eaca734cd57d64f153a2961781218098c27
johnohhh1 <john.olenski@gmail.com>
2025-07-11 01:45:56 -0400

    Fix API route export format for Next.js compatibility

--- pages/api/basic-test.js
+++ pages/api/basic-test.js
@@ -1,4 +1,4 @@
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Username, X-Password');
@@ -15,4 +15,4 @@ module.exports = function handler(req, res) {
     method: req.method,
     timestamp: new Date().toISOString()
   });
-}; 
+} 
